486d527c8fa8a4df1487bc811cf0db8f86a43afe
nickshaw1 <nickshawcontact@gmail.com>
2026-04-16 03:49:20 +0100

    Block AI scrapers in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,41 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://stormontwatch.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: [
+          'GPTBot',
+          'ChatGPT-User',
+          'CCBot',
+          'anthropic-ai',
+          'ClaudeBot',
+          'Claude-Web',
+          'Google-Extended',
+          'FacebookBot',
+          'Amazonbot',
+          'Applebot-Extended',
+          'Bytespider',
+          'cohere-ai',
+          'Diffbot',
+          'ImagesiftBot',
+          'magpie-crawler',
+          'omgili',
+          'omgilibot',
+          'PetalBot',
+          'Scrapy',
+          'Timpibot',
+          'YouBot',
+          'PerplexityBot',
+          'AI2Bot',
+          'Kangaroo Bot',
+        ],
+        disallow: '/',
+      },
+    ],
+    sitemap: 'https://www.stormontwatch.com/sitemap.xml',
   }
 }
